e8799f634266a69413a0ea60b123aa3cb97c8700
Denisse Suarez <den.sua.j@gmail.com>
2022-03-21 11:04:44 +0100

    Add test using testing library

--- src/components/__tests__/ParentComponent.spec.js
+++ src/components/__tests__/ParentComponent.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import ParentComponent from '@/components/ParentComponent.vue'
+import { render } from '@testing-library/vue'
 
 describe('ParentComponent', () => {
     it('show message with shallowMount', () => {
@@ -10,8 +11,14 @@ describe('ParentComponent', () => {
 
     it('show message with mount', () => {
         const wrapper = mount(ParentComponent)
-
+        
         expect(wrapper.find('.spanInChild').exists()).toBe(true)
         expect(wrapper.find('.spanInChild').text()).toBe('Message from child component')
+    })
+
+    it('show message with render', () => {
+        const { getByText } = render(ParentComponent)
+        
+        getByText('Message from child component')
     })
 })
